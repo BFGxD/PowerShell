2f69dabc64bf4e4712d929ee56190da49bfeac3f
BFGxD <w20kx1@gmail.com>
2024-06-06 17:52:33 +0300

    Add files via upload

---
+++ NewUser v1.6.1.ps1
@@ -0,0 +1,295 @@
+﻿#Выбор: запуск на сервере Exchange или будет подключаться к указанному серверу Exchange, аунтификация по текущей сессии (можно и с запросом данных сделать)
+$Se = Read-Host "Удаленная сессия Y/N"
+
+if (($se -eq 'Y') -or ($se -eq 'y')){
+#Подключение удаленной сессии powershel на exchange
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://exchange01.domain.com/powershell/ -Authentication Kerberos
+Import-PSSession $Session -DisableNameChecking -AllowClobber
+}else{Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn}
+
+Import-Module ActiveDirectory
+
+$pwds = "Какой то пароль"
+#Вывод данных по активным почтовым базам
+Get-MailboxDatabase -Status | select-object Name,Server,DatabaseSize,Mounted | Sort-Object -Property databasesize | where {$_.name-like "mailbox0*"} | ft -AutoSize
+Write-Host " "
+#Выбираем базу для создания учетки
+$NDB = Read-Host "Введите номер почтовой базы (последняя цифра)"
+$mbdb = "mailbox0$NDB"
+$pwd = ConvertTo-SecureString $pwds -AsPlainText -Force
+
+$fio  = ""
+$auto = "99"
+while ($auto -ne "0") {
+$sam  = ""
+#Выбираем тип создания уч. записей ( через переменную auto, дальше на основе выбора скрипт будет брать либо из файла данные, либо просить ввести
+Write-Host " "
+Write-Host "===========================Меню создания уч. записи=============================="
+Write-Host " "
+Write-Host "1.  Ручной ввод
+2.  Загрузка из файла
+0.  Выход 
+"
+$auto = Read-Host "Введите значение"
+while ($auto -notmatch "(0|1|2)" ) {
+    $auto = Read-Host "Номера нет в списке, укажите с 0 по 2"
+}
+if ($auto -ne "0") {
+while ($sam -eq "") {
+#Путь к файлу с данными по учеткам 
+Import-CSV "\\server.domain.com\common4$\1C_Common\Upload\mail\NewADusers.utf8" -Delimiter ";" -Encoding UTF8 | ForEach-Object {
+
+$mail = ""
+$sam  = ""
+$fio  = ""
+
+if ($auto -eq "1") {
+Write-Host " "
+Write-Host "=========================В какое OU поместить уч. запись======================="
+Write-Host " "
+Write-Host "1.  Главный офис
+2.  Второй офис
+3.  Виртуальная машина
+4.  Сф
+5.  Севастополь
+6.  БГУ
+7.  Геленджик
+8.  Гор.
+9.  Кемерово
+10. Пермь
+11. Владивосток
+12. Магадан
+13. S7
+14. Калинград
+15. Сторонние компании.
+0. Проекты
+"
+$OUid = Read-Host "Введите значение"
+
+while ($ouid -notmatch "(0|1|2|3|4|5|6|7|8|9|10|11|12|13|15)" ) {
+    $OUid = Read-Host "Номера нет в списке, укажите с 0 по 15"
+}} else {$OUid = $_.ouid}
+
+#Список общих пк для входа
+$NewUsWork= "dc01,dc02,dc03,main,ADFS01,CAS,EXCHANGE01,EXCHANGE02,EXCHANGE03"
+#Ввод ФИО сотрудника, с проверкой пустого поля 
+if ($auto -eq "1") {$fio = Read-Host "Введите ФИО сотрудника"} else {$fio = $_.fio}
+$i=0;while ($fio -eq "")  {If ($fio -eq ""){if ($i -lt 3) {$fio = Read-Host "ФИО не должно быть пустым, введите ФИО";$i++}else{break}}}
+#Ввод имени учетки, с провркой на пустое поле
+if ($auto -eq "1") {$sam = Read-Host "Введите SamAccountName"} else {$sam = $_.sam}
+$i=0;while ($sam -eq "")  {If ($sam -eq ""){if ($i -lt 3) {$sam = Read-Host "Имя не может быть пустым, введите имя";$i++}else{break}}}
+if ($sam -eq "") {Write-Host "Имя учетной записи не может быть пустым, создание записи отменено";break}
+#Ввод имени пк для входа
+if ($auto -eq "1") {$pc = Read-Host "Введите имя ПК"} else {$pc = $_.pc}
+#По выбору выше выбирает нужную OU
+If ($OUid -eq "1" ) { $ou = "domain.com/Domain Users and User's Computers/Domain Users/Internet/Normal" }
+If ($OUid -eq "2" ) { $ou = "domain.com/Projects/S Plaza/Users" }
+If ($OUid -eq "3" ) { $ou = "domain.com/Domain Users and User's Computers/Domain Users/Internet/Horizon" }
+If ($OUid -eq "4" ) { $ou = "domain.com/Projects/Sf/Users" }
+If ($OUid -eq "5" ) { $ou = "domain.com/Projects/Sevastopol/Users" }
+If ($OUid -eq "6" ) { $ou = "domain.com/Projects/Rostov Gidrouzel (BGU)/Users"}
+If ($OUid -eq "7" ) { $ou = "domain.com/Projects/Gelendzhik/Users"}
+If ($OUid -eq "8" ) { $ou = "domain.com/Projects/Gorka/Users"}
+If ($OUid -eq "9" ) { $ou = "domain.com/Projects/Kemerovo/Users" }
+If ($OUid -eq "10" ) {$ou = "domain.com/Projects/Perm/Users"}
+If ($OUid -eq "11" ) {$ou = "domain.com/Projects/Vladivostok/Users" }
+If ($OUid -eq "12" ) {$ou = "domain.com/Projects/Magadan/Users"}
+If ($OUid -eq "13" ) {$ou = "domain.com/Projects/S7/Users" }
+If ($OUid -eq "14" ) {$ou = "domain.com/Projects/Kaliningrad/Users" }
+If ($OUid -eq "15" ) {$ou = "domain.com/Domain Users and User's Computers/Domain Users/Internet/Horizon" }
+If ($OUid -eq "0" ) { $ou = "domain.com/Projects" }
+
+#Поиск совпадений в AD
+
+    $userF = (Get-ADUser -Filter {Name -like $fio}).Name
+    $userS = (Get-ADUser -Filter {SamAccountName -like $sam}).SamAccountName
+     
+    If (($userF -eq $fio) -or ($userS -eq $sam)) {
+    if ($userF -eq $fio) {Write-Host "ФИО: $fio - уже используется"}
+    if ($userS -eq $sam) {Write-Host "Имя: $sam - уже используется"} 
+    $n = Read-Host "Продолжить Y/N";if (($n -eq "N") -or ($n -eq "n")) {break} } else {Write-Host "ФИО: $fio - Свободно";Write-Host "Имя: $sam - Свободно"}
+
+#Проверка ФИО   
+    while ($userF -eq $fio)  {
+        $fio = Read-Host "'$fio' - используется, введите другое ФИО сотрудника"
+        If ($fio -eq ""){break}else{$userF = (Get-ADUser -Filter {Name -like $fio}).Name}}
+
+        #If ($fio -eq ""){Write-Host "ФИО не может быть пустым, завершение цикла";break}else{Write-Host "ФИО: $fio - свободно"}
+
+#Проверка логина
+   
+
+    while ($userS -eq $sam) {
+        $name = (Get-ADUser -Filter {SamAccountName -like $sam}).name  
+        $sam = Read-Host "'$sam' используется в '$name', введите другое имя"
+        If ($sam -eq ""){Write-Host "Имя не может быть пустым, завершение цикла";break}else{$userS = (Get-ADUser -Filter {SamAccountName -like $sam}).SamAccountName}}        
+
+        #If ($sam -eq ""){break}else{Write-Host "Logon: $sam - свободно"}
+
+$alias = $sam
+ 
+#Разбивка ФИО на состовляющие (фамилия и имя, для записи в соответствующие поля)
+    $fio2 = $fio.split("")
+    $fname = $fio2[1]
+    $lname = $fio2[0]
+
+$upn = $sam + "@domain.com"
+
+#Для сторонних компаний есть выбор создать учетку с почтой или без
+if ($OUid -eq "15" ) {if ($auto -eq "1") {$mail = Read-Host "Создать почту Y/N"}else{$mail = $_.mail}}
+
+#Создание учетки
+if (($OUid -in 0..14 ) -or ($mail -like "Y") -or ($mail -like "y")) {New-Mailbox -name $fio -userprincipalname $upn -Alias $alias -OrganizationalUnit $ou -SamAccountName $sam -FirstName $fname -LastName $lname -Password $pwd –Database $mbdb}
+else {$ou = "OU=Horizon,OU=Internet,OU=Domain Users,OU=Domain Users and User's Computers,DC=domain,DC=com";New-ADUser -Name $fio -DisplayName $fio -GivenName $fname -Surname $lname -UserPrincipalName $upn -SamAccountName $SAM -Path $OU -AccountPassword (ConvertTo-SecureString $pwds -AsPlainText -force) -Enabled $true}
+
+#Ждем обновление сервера, после создания учетки, если не выждать то ситема выдаст, что учетка не найдена при добавлении параметров 
+Start-Sleep -Seconds 16
+
+#добавление к списку стандартных пк то, что ввели
+$NewUsWork += "," + $pc
+
+#Если создаем учетную запись VM то добавляется группа и серверы в список для входа
+if (($ouid -eq "3") -or ($ouid -eq "15"))
+    {
+        $NewUsWork += "," + "hcon01" + "," + "hcon02" 
+        Add-AdGroupMember -Identity "HORIZON Block policy for Users and Computers" -members $sam
+        Write-Host " "
+        Write-Host "Добавлена группа:"
+        write-host "HORIZON Block policy for Users and Computers"
+        #Если пункт сторонняя компания, то нужно еще ввести название компании и можно через сколько дней отключить учетку (задасть дату отключения)
+	if ($ouid -eq "15") {
+        if ($auto -eq "1") {$company = Read-Host "Введите название компании";$discrip = "";$discrip = Read-Host "Введите описание"; $dayYN = Read-Host "Установить дату отключения Y/N";if (($dayYN -eq 'Y')-or($dayYN -eq 'y')){$dayof=Read-Host "Укажите через сколько дней отключить"}}else{$company = $_.company;$discrip = $_.discrip;$dayYN = $_.dayyn;$dayof = $_.dayoff}
+        if(($dayYN -eq "Y") -or ($dayYN -eq "y")){$dayoff = (Get-Date).AddDays(1+($dayof)).ToString('dd-MM-yyyy')}}
+    }
+
+#Если создаем учетную в Пермь то добавляем RODC10
+if ($ou -eq "domain.com/Projects/Perm/Users")
+    {
+        Add-AdGroupMember -Identity "Allowed RODC Password Replication RODC10" -members $sam
+        Write-Host " "
+        Write-Host "Добавлена группа:"
+        write-host "Allowed RODC Password Replication RODC10"
+    }
+
+#Если создаем учетную в Геленджик то добавляем RODC11
+if ($ou -eq "domain.com/Projects/Gelendzhik/User")
+    {
+        Add-AdGroupMember -Identity "Allowed RODC Password Replication RODC11" -members $sam
+        Write-Host " "
+        Write-Host "Добавлена группа:"
+        write-host "Allowed RODC Password Replication RODC11"
+    }
+
+#Если создаем учетную в Калинград то добавляем RODC14
+if ($ou -eq "domain.com/Projects/Kaliningrad/Users")
+    {
+        Add-AdGroupMember -Identity "Allowed RODC Password Replication RODC14" -members $sam
+        Write-Host " "
+        Write-Host "Добавлена группа:"
+        write-host "Allowed RODC Password Replication RODC14"
+    }
+
+#Если создаем учетную в Владивосток то добавляем RODC15
+if ($ou -eq "domain.com/Projects/Vladivostok/Users")
+    {
+        Add-AdGroupMember -Identity "Allowed RODC Password Replication RODC15" -members $sam
+        Write-Host " "
+        Write-Host "Добавлена группа:"
+        write-host "Allowed RODC Password Replication RODC15"
+    }
+
+#Если создаем учетную в Кемерово то добавляем RODC17
+if ($ou -eq "domain.com/Projects/Kemerovo/Users")
+    {
+        Add-AdGroupMember -Identity "Allowed RODC Password Replication RODC17" -members $sam
+        Write-Host " "
+        Write-Host "Добавлена группа:"
+        write-host "Allowed RODC Password Replication RODC17"
+    }
+
+#Если создаем учетную в Севастополь то добавляем RODC18
+if ($ou -eq "domain.com/Projects/Sevastopol/Users")
+    {
+        Add-AdGroupMember -Identity "Allowed RODC Password Replication RODC18" -members $sam
+        Write-Host " "
+        Write-Host "Добавлена группа:"
+        write-host "Allowed RODC Password Replication RODC18"
+    }
+
+#Если создаем учетную в Сфера то добавляем RODC19
+if ($ou -eq "domain.com/Projects/Sf/Users")
+    {
+        Add-AdGroupMember -Identity "Allowed RODC Password Replication RODC19" -members $sam
+        Write-Host " "
+        Write-Host "Добавлена группа:"
+        write-host "Allowed RODC Password Replication RODC19"
+    }
+
+#Если создаем учетную в Симонов то добавляем RODC23
+if ($ou -eq "domain.com/Projects/S Plaza/Users")
+    {
+        Add-AdGroupMember -Identity "Allowed RODC Password Replication RODC23" -members $sam
+        Write-Host " "
+        Write-Host "Добавлена группа:"
+        write-host "Allowed RODC Password Replication RODC23"
+    }
+#Записываем пк для входа учетке
+Set-ADUser -Identity $sam -Replace @{userWorkstations=$NewUsWork} 
+
+#Установка региональных настроек
+
+if (($OUid -in 0..14 ) -or (($mail -like "Y") -or ($mail -like "y"))) {Set-Mailbox $sam -EmailAddresses @{Add="$sam@domain.com"};Set-Mailbox $sam -PrimarySmtpAddress "$sam@domain.com" -EmailAddressPolicyEnabled $false
+if((($ouid -in 0..8)-or($ouid -in 13..15))-and(-not($ouid -eq 14))){Set-MailboxRegionalConfiguration -Identity $sam -TimeZone "Russian Standard Time" -Language ru-ru -LocalizeDefaultFolderName:$true -DateFormat dd.MM.yyyy -TimeFormat H:mm}else{
+if($OUid -eq 9){Set-MailboxRegionalConfiguration -Identity $sam -TimeZone "North Asia Standard Time" -Language ru-ru -LocalizeDefaultFolderName:$true -DateFormat dd.MM.yyyy -TimeFormat H:mm}else{
+if($OUid -eq 10){Set-MailboxRegionalConfiguration -Identity $sam -TimeZone "Ekaterinburg Standard Time" -Language ru-ru -LocalizeDefaultFolderName:$true -DateFormat dd.MM.yyyy -TimeFormat H:mm}else{
+if($OUid -eq 11){Set-MailboxRegionalConfiguration -Identity $sam -TimeZone "Vladivostok Standard Time" -Language ru-ru -LocalizeDefaultFolderName:$true -DateFormat dd.MM.yyyy -TimeFormat H:mm}else{
+if($OUid -eq 14){Set-MailboxRegionalConfiguration -Identity $sam -TimeZone "Kaliningrad Standard Time" -Language ru-ru -LocalizeDefaultFolderName:$true -DateFormat dd.MM.yyyy -TimeFormat H:mm}else{
+if($OUid -eq 12){Set-MailboxRegionalConfiguration -Identity $sam -TimeZone "Magadan Standard Time" -Language ru-ru -LocalizeDefaultFolderName:$true -DateFormat dd.MM.yyyy -TimeFormat H:mm}}}}}}}
+
+#Запись в учетку для сторонней компании: название компании и дату отключения
+
+if (($OUid -eq 15 ) -and ($discrip -eq "")) {$discrip = $company}
+if (($OUid -eq 15 ) -and (($dayYN -eq "Y") -or ($dayYN -eq "y"))) {set-aduser -Identity $sam -Company $company -Description $discrip -AccountExpirationDate $dayoff} else {if ($OUid -eq 15 ) {set-aduser -Identity $sam -Company $company -Description $discrip}}
+
+#Создание папки пользователя на FS
+$fs = '\\fs\users$\' + $sam
+New-Item -Path $fs -ItemType Directory
+#Назначение прав на папку для пользователя 
+$UserPath = "domain\" + $sam
+$Rights = "Read, ReadAndExecute, ListDirectory, Write, Modify"
+$InheritSettings = "Containerinherit, ObjectInherit"
+$PropogationSettings = "None"
+$RuleType = "Allow"
+$acl = Get-Acl $fs
+$perm = $UserPath, $Rights, $InheritSettings, $PropogationSettings, $RuleType
+$rule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $perm
+$acl.SetAccessRule($rule)
+$acl | Set-Acl -Path $fs
+#Снова ждем обновления записанных данных
+Start-Sleep -Seconds 12
+#Выводит на экран, что записал учеткам
+if (($OUid -in 1..14 ) -or (($mail -like "Y") -or ($mail -like "y"))) {
+Get-MailboxRegionalConfiguration -Identity $sam | fl timezone
+Write-Host "Почтовые адреса:"
+write-host "Все адреса:" (get-Mailbox -Identity $sam ).EmailAddresses
+write-host "Гравный адрес:" (get-Mailbox -Identity $sam ).PrimarySmtpAddress}
+Write-Host " "
+Write-Host "Список ПК для входа:"
+$account=Get-ADUser -Filter {SamAccountName -eq $sam} -properties *
+write-host $account.userWorkstations
+Write-Host " "
+#Для центрального офиса проверяет, если указанный пк в начальной OU То переносит его в нужную
+if ($ou -eq "domain.com/Domain Users and User's Computers/Domain Users/Internet/Normal")
+{if ($pc){
+$oupc = (Get-ADComputer -Filter {Name -like $pc}).DistinguishedName
+$oupc1 = "CN="+$pc+",CN=Computers,DC=domain,DC=com" 
+if ($oupc -eq $oupc1)
+{Get-ADComputer -Identity $PC | Move-ADObject -TargetPath "OU=Internet,OU=Domain Computers,OU=Domain Users and User's Computers,DC=domain,DC=com"
+Write-Host "'$PC' перенесен в domain.com/Domain Users and User's Computers/Domain Computers/Internet"
+} Else {Write-Host "'$PC' находится в '$oupc'"}}}
+if ($auto -ne 2){break}
+#Write-Host " ";Write-Host "=================================================================================";
+}
+}}
+#При выходе проверяет, если было удаленное подключение, то закрывает сессию
+}if (($se -eq 'Y') -or ($se -eq 'y')){Remove-PSSession $Session};break
---
+++ Poisk i udalenie rassilok.ps1
@@ -0,0 +1,297 @@
+﻿#Скрипт автоматической очистки ящиков пользователей от массовых рассылок по компании. Есть эталонный ящик включеный во все крупные группы рассылок. На основе данных о письмах в этом ящике происходит поик и удаление писем в заданном диапозоне времени. Шаг день, 30 дней назад. Все динамиское: кол-во баз, пользователей, писем. После выполнения скрипт пришлет отчет о проделанной работе.
+
+Import-Module ActiveDirectory
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://exchange01.domain.com/powershell/ -Authentication Kerberos
+Import-PSSession $Session -DisableNameChecking -AllowClobber
+
+$FilePath = '\\cluster.domain.com\common4$\1C_Common\Upload\mail\logs\spam' 
+New-PSDrive -Name file -PSProvider FileSystem -Root $FilePath
+
+$namelog = "Letters"
+$date_start = 31 
+$date_to = 30
+$date_stop = 30 # на каком дне остановиться
+$kdate = 1 # шаг в днях
+
+$log = "file:\$namelog.csv"
+
+If (Test-Path $log )  {
+$copy = 'file:\backup\'+$namelog+' '+(get-date).ToString('dd MM yyyy')+'.csv'
+cpi $log $copy
+}
+
+$logSuccessResults = "file:\$namelog"+"Results.csv"
+
+If (Test-Path $logSuccessResults )  {
+$copy = 'file:\backup\'+$namelog+'Results '+(get-date).ToString('dd MM yyyy')+'.csv'
+cpi $logSuccessResults $copy
+}
+
+set-Content -Path $filepath'\'$namelog".csv" -Value 'Отправитель;тема;Найдено;Размер(MB);Поиск;Удаление'
+set-Content -Path $filepath'\'$namelog"Results.csv" -Value 'Отправитель;тема;Получатель;Найдено;Размер(МБ)'
+
+
+New-ComplianceSearch -Name $namelog -ExchangeLocation newsletters@stg.local -ContentMatchQuery "(Received:$((get-date).AddDays(-$date_start).ToString('yyyy-MM-dd'))..$((get-date).AddDays(-$date_to).ToString('yyyy-MM-dd')))"
+Start-ComplianceSearch -Identity $namelog
+$seach = Get-ComplianceSearch -Identity $namelog
+while ($seach.status -ne "Completed") {Start-Sleep -Seconds 15;$seach = Get-ComplianceSearch -Identity $namelog}
+
+if ((Get-ComplianceSearch $namelog).items -gt 0) {
+
+New-ComplianceSearchAction -SearchName $namelog -Preview -Confirm:$false
+
+$find_letter = $namelog + "_Preview"
+$seach = Get-ComplianceSearchAction -Identity $find_letter
+while ($seach.status -ne "Completed") {Start-Sleep -Seconds 15;$seach = Get-ComplianceSearchAction -Identity $find_letter}
+(Get-ComplianceSearchAction $find_letter -Details).Results -replace '{', "Location,Sender,Subject,Type,Size,ReceivedTime,DataLink`r`n" -replace '}' -replace 'Location: ' -replace '; Sender: ', ',' -replace '; Subject: ', ',' -replace '; Type: ', ',' -replace '; Size: ', ',' -replace '; Received Time: ', ',' -replace '; Data Link: ', ',' -replace ",`r`n", "`r`n" | Out-File c:\temp\letters.csv
+remove-ComplianceSearch -Identity $namelog -Confirm:$false
+[Array]$Letters_list = Import-CSV "c:\temp\letters.csv" -Delimiter "," -Encoding UTF8  
+
+$i = 1
+$s = ""
+$mb = 0
+$items = 0
+$ii = 0
+
+$ii = 0
+$baseuserall = ""
+
+$base = Get-MailboxDatabase -Status | select-object Name | where {$_.name-like "mailbox0*"}
+$groupnames = ""
+
+$l = "["
+$lx = ""
+$shkala = 30 #ширина шкалы
+for ($i=0;$i -lt $shkala;$i++){$lx += " "}
+$ln ="]"
+$i = 0
+$sb = ""
+
+$base.ForEach({if($sb -eq ""){$sb+=$_.name}else{$sb+=","+$_.name}})
+
+
+$base.foreach({
+
+$r=0
+if ($i -eq 0){$k = 0}else{$k = $i / $base.Count * 100;$lx="";for ($o =1; $o -le $shkala; $o++ ){ if ($o -le $k/10*($shkala/10)){$lx +="*"}else{$lx +=" "}}}
+$i++
+#clear
+$NewGroupName = $_.name+'users'
+
+write-host "Список баз:$sb"
+write-host ""
+write-host "Общий прогресс:$l$lx$ln $k%  Заполнение группы:$NewGroupName"
+write-host ""
+    
+    if($groupnames -eq ""){$groupnames="$NewGroupName@stg.local"}else{$groupnames+=",$NewGroupName@stg.local"}
+    if (((Get-DistributionGroup $NewGroupName -ErrorAction 'SilentlyContinue').IsValid) -eq $true) {} else {
+        New-DistributionGroup -Name $NewGroupName -SamAccountName $NewGroupName -OrganizationalUnit “domain.com/Domain Groups mail” -DisplayName $NewGroupName -Alias $NewGroupName  -Type "Security" -ManagedBy secofr07
+        start-sleep -Seconds 15
+        Set-DistributionGroup -Identity $NewGroupName -HiddenFromAddressListsEnabled $true 
+    }
+    $bc = (Get-Mailbox -Database $_.name -ResultSize:Unlimited).count
+    $gc = (Get-DistributionGroupMember -id $NewGroupName -resultsize:unlimited).count
+    if ($bc -eq $gc) {} else {
+    $exloc = Get-DistributionGroupMember -id exdelmailspam@stg.local
+    $baseuser = Get-Mailbox -Database $_.name -resultsize unlimited
+    Update-DistributionGroupMember –Identity $NewGroupName –Member (Import-Csv "\\cluster.domain.com\common4$\1C_Common\Upload\mail\0.csv").username -confirm:$false
+    
+    foreach ($member in $baseuser){$iq = 0;foreach ($member1 in $exloc){If ($member1.SamAccountName -eq $member.alias) {$iq = 1}}if ($iq -eq 0) {$memberid=$member.alias;Add-DistributionGroupMember $NewGroupName -member $memberid}}}
+
+})
+
+$base.foreach({
+$NewGroupName = $_.name+'users'
+if($groupnames -eq ""){$groupnames="$NewGroupName@stg.local"}else{$groupnames+=",$NewGroupName@stg.local"}
+})
+
+
+$baseuseralllist = $groupnames.split(',')
+
+write-host "Список баз:$sb"
+if ($i -eq 0){$k = 0}else{$k = $i / $base.Count * 100;$lx="";for ($o =1; $o -le $shkala; $o++ ){ if ($o -le $k/10*($shkala/10)){$lx +="*"}else{$lx +=" "}}}
+write-host "Общий прогресс:$l$lx$ln $k%"
+$s = ""
+$i = 1
+
+$Letters_list.foreach({
+    $ii = 0
+    $senderk = $_.sender                 
+    if ($senderk -eq "Lotus01") {$from = "from:Lotus01/domain@domain.com"} else {$from = "from:"+(Get-ADuser -Filter {displayName -like $senderk}).userPrincipalName}
+    $subj = "Subject:"+$_.Subject
+    $base.foreach({
+        $basename = $_.name
+        New-ComplianceSearch -Name "$Namelog$basename$i" -ExchangeLocation $baseuseralllist[$ii] -ContentMatchQuery "(Received:$((get-date).AddDays(-$date_start).ToString('yyyy-MM-dd'))..$((get-date).AddDays(-$date_to).ToString('yyyy-MM-dd')) AND ($from) AND ($subj))"
+        if($s -eq ""){$s+="$Namelog$basename$i"}else{$s+=",$Namelog$basename$i"}
+        $i++
+        $ii++
+    })
+})
+
+
+$list = $s.Split(',')
+
+$l = "["
+$lx = ""
+$shkala = 30 #ширина шкалы
+for ($i=0;$i -lt $shkala;$i++){$lx += " "}
+$ln ="]"
+$i = 0
+$iii = 0
+$ll = 0
+$jh = 0
+
+$list.foreach({
+
+$r=0
+
+if ($jh -eq 4) {$ll++;$jh=0}
+
+
+if ($i -eq 0){$k = 0}else{$k = $i / $list.Count * 100;$lx="";for ($o =1; $o -le $shkala; $o++ ){ if ($o -le $k/10*($shkala/10)){$lx +="*"}else{$lx +=" "}}}
+
+#clear
+
+write-host "Список задач:$s"
+write-host ""
+write-host "Общий прогресс:$l$lx$ln $k%  Текущая задача:$_"
+write-host ""
+
+Start-ComplianceSearch -Identity $_
+$seach = Get-ComplianceSearch -Identity $_
+while ($seach.status -ne "Completed") {Start-Sleep -Seconds 15;$seach = Get-ComplianceSearch -Identity $_}
+$DataSearch = Get-ComplianceSearch -Identity $_
+(Get-ComplianceSearch $_).SuccessResults -replace '{', "Location,Item,Size`r`n" -replace '}' -replace 'Location: ' -replace ', Item count: ', ',' -replace ', Total size: ', ',' -replace ",`r`n", "`r`n" | Out-File c:\temp\Findlist.csv
+
+if ((Get-ComplianceSearch $_).items -eq 0) {$dels = "Completed"} else {
+
+New-ComplianceSearchAction -SearchName $_ -Purge -PurgeType SoftDelete -Confirm:$false
+#New-ComplianceSearchAction -SearchName $_ -Preview -Confirm:$false
+$del_letter = $_ + "_Purge"
+#$del_letter = $_ + "_Preview"
+$seach = Get-ComplianceSearchAction -Identity $del_letter
+while ($seach.status -ne "Completed") {Start-Sleep -Seconds 15;$seach = Get-ComplianceSearchAction -Identity $del_letter}
+$del = Get-ComplianceSearchAction -Identity $del_letter #| FL name,status  
+$dels = $Del.Status
+}
+remove-ComplianceSearch -Identity $_ -Confirm:$false
+
+$dsi = $DataSearch.Items
+$dsm = [math]::Round($DataSearch.size/1MB,2)
+$dss = $DataSearch.Status
+$namereq = $_
+[array]$res = Import-CSV "C:\Temp\Findlist.csv" -Delimiter "," -Encoding UTF8
+
+$sendn = $Letters_list[$ll].sender
+$subn = $Letters_list[$ll].Subject
+
+$res.ForEach({
+    if ($_.item -gt 0){
+        $us = $_.location
+        $item = $_.item
+        $smb = [math]::Round($_.size/1MB,2)
+        Add-Content -Path $filepath'\'$namelog'Results.csv' -Value "$sendn;$subn;$us;$item;$smb"}
+})
+
+Add-Content -Path $filepath'\'$namelog".csv" -Value "$sendn;$subn;$dsi;$dsm;$dss;$dels"
+
+$mb += $DataSearch.size/1MB
+$items += $DataSearch.Items
+if ($list[$list.Count-1] -eq $_) {$mb = [math]::Round($MB); Add-Content -Path $filepath'\'$namelog".csv" -Value "$namelog;$Items;$MB;-;-"}
+
+$i++
+$jh++
+})
+
+#clear
+write-host "Задача: $namelog | Удалено: $items писем | Размер: $mb MB |общий прогресс: 100%"
+
+} else {remove-ComplianceSearch -Identity $namelog -Confirm:$false;Add-Content -Path $filepath'\'$namelog".csv" -Value "Нет писем;Нет писем;0;0;Complite;Complite";write-host "Задача: $namelog | Удалено: 0 | Размер: 0 |общий прогресс: 100%";$Letters_list = "";$mb = 0;$items = 0} 
+
+
+$sr = "Дата проверки: "
+$sr += (get-date).AddDays(-$date_start).ToString('yyyy-MM-dd')
+$srk = "<h1>Результаты очистки:</h1>
+<p>&nbsp;</p>
+<table width='100%'>
+<tbody>
+<tr>
+<td style='width: 1%;'>
+<p>&nbsp;</p>
+</td>
+<td style='width: 6.9149%;'>
+<p><strong>Дата проверки:</strong></p>
+</td>
+<td style='width: 15.0851%;'>
+<p>$sr</p>
+</td>
+</tr>
+<tr>
+<td style='width: 1%;'>
+<p>&nbsp;</p>
+</td>
+<td style='width: 6.9149%;'>
+<p><strong>Найдено писем:</strong></p>
+</td>
+<td style='width: 15.0851%;'>
+<p>$items</p>
+</td>
+</tr>
+<tr>
+<td style='width: 1%;'>
+<p>&nbsp;</p>
+</td>
+<td style='width: 6.9149%;'>
+<p><strong>Общий размер:</strong></p>
+</td>
+<td style='width: 15.0851%;'>
+<p>$mb МБ</p>
+</td>
+</tr>
+<tr>
+<td style='width: 27%;' colspan='3'>
+<p>&nbsp;</p>
+</td>
+</tr>"
+
+$iii = 1
+$Letters_list.ForEach({
+$srk += "<tr>
+<td style='width: 1%;'>
+<p>&nbsp;</p>
+</td>
+<td style='width: 6.9149%;'>
+<p><strong>Запрос №"+$iii+":</strong></p>
+</td>
+<td style='width: 15.0851%;'>
+<p>Отправитель:&nbsp;"+$_.Sender+"</p>
+</td>
+<td style='width: 30.0851%;'>
+<p>Тема:&nbsp;"+$_.Subject+"</p>
+</td>
+</tr>"
+$iii++
+
+})
+
+
+$srk += "</tbody>
+</table>"
+
+
+$result_recipient = 'it@domain.com' # Ящик получателя отчета
+$result_sender = 'Delete.letters@domain.com' # Ящик отправителя отчета
+$smtp_server = 'cas.domain.com' # SMTP-серввер для отправки отчета
+$email_subject = 'Удаление рассылок' # Тема отправляемого письма
+
+$email_body = $srk
+
+$filepathl = $filepath+'\'+$namelog+".csv"
+$filepathr = $filepath+'\'+$namelog+'Results.csv'
+
+Send-MailMessage -SmtpServer $smtp_server -To $result_recipient -From $result_sender -Subject $email_subject -Body $email_body -BodyAsHtml -Encoding 'UTF8' -Attachments $filepathl,$filepathr
+
+remove-PSDrive -Name file 
+
+Remove-PSSession $Session 
